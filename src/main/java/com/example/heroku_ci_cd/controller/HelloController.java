773f13e1c46f745a4da01f362168eb8c3f6bcb1a
cloudwi <cloudwi@naver.com>
2022-08-29 16:39:18 +0900

    헤로쿠 배포 최종

--- src/main/java/com/example/heroku_ci_cd/controller/HelloController.java
+++ src/main/java/com/example/heroku_ci_cd/controller/HelloController.java
@@ -1,15 +1,16 @@
 package com.example.heroku_ci_cd.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("")
     public String hello() {
-        return "Hello World";
+        return "home";
     }
+
+
 }
